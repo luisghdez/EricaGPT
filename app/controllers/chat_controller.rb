b5778b9ef5ca82d62a5d5d81617bc86f6a30bca1
Luis Hernandez <1gluishernandez@gmail.com>
2023-04-02 11:12:37 -0600

    changed model from davinci to gpt 3.5

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,13 +6,13 @@ class ChatController < ApplicationController
 
     if params[:user_input].present?
       # User has submitted a message, generate a response from OpenAI
-      client = OpenAI::Client.new(access_token: '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')
+      client = OpenAI::Client.new(access_token: "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")
 
       context = "YOUR NAME IS ERICA ACT AS MY FINANCIAL ADVISOR, AND FRIEND.
 
       Help users reach their desired purchase goal efficiently and personally. Analyze their financial information and suggest a tailored savings plan. Consider factors like age, spending habits, and demographics when crafting your response.
 
-      INPUTS:
+      USER INPUTS:
 
       Name: Luis
       Age: 20
@@ -27,23 +27,27 @@ class ChatController < ApplicationController
       RESPONSE FORMAT EXAMPLE:
       ""[Name], by [adjusting spending/saving $X daily/weekly] for the next [Y months/weeks], you can afford your desired item without repercussions. Would you like to set up a '[Goal] Savings Account' and automatically transfer $X daily/weekly for the duration of the plan?""
 
-      EXAMPLE:
-      Samuel, by cutting entertainment spending by $10 daily for 2 months, you can afford your desired item without repercussions. Would you like to set up a 'New Shoes Savings Account' and automatically transfer $10 daily for the next 2 months?
+      Make sure to give personalized data, follow the response format example to the letter, but make sure to change it based on the different output.
       "
 
       prompt = "#{context} #{params[:user_input]}"
 
-      response = client.completions(
+      response = client.chat(
         parameters: {
-          model: "text-davinci-003",
-          prompt: prompt,
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: 'user',
+              content: prompt
+            }
+          ],
           max_tokens: 150
 
         })
 
       user_message = params[:user_input]
-      bot_message = response['choices'].first['text'].strip
-      bot_message.gsub!(/[^a-zA-Z\s]/, '')
+      bot_message = response.parsed_response['choices'][0]['message']['content']
+      bot_message.gsub!(/[^a-zA-Z.\s]/, '')
 
       session[:messages] << {user: user_message, bot: bot_message}
     else
